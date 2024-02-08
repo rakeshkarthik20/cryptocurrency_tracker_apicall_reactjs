9f3e0ac2831d8a00144b892c12e6d7099147bd9f
Rakesh Karthik <rakeshkarthikgrk@gamil.com>
2024-02-08 19:48:55 +0000

    first commit

---
+++ src/components/CryptocurrenciesList/index.js
@@ -0,0 +1,45 @@
+import {Component} from 'react'
+
+import CryptocurrencyItem from '../CryptocurrencyItem'
+
+import './index.css'
+
+class CryptocurrenciesList extends Component {
+  renderCryptocurrenciesView = () => {
+    const {cryptocurrenciesData} = this.props
+
+    return (
+      <div className="cryptocurrencies-list-container">
+        <div className="list-header">
+          <p className="list-coin-type-heading">Coin Type</p>
+          <div className="usd-and-euro-values-container">
+            <p className="list-coin-value-heading">USD</p>
+            <p className="list-coin-value-heading">EURO</p>
+          </div>
+        </div>
+
+        <ul className="cryptocurrencies-list">
+          {cryptocurrenciesData.map(each => (
+            <CryptocurrencyItem key={each.id} cryptocurrencyDetails={each} />
+          ))}
+        </ul>
+      </div>
+    )
+  }
+
+  render() {
+    return (
+      <div className="cryptocurrencies-container">
+        <h1 className="heading">Cryptocurrency Tracker</h1>
+        <img
+          className="cryptocurrency-img"
+          src="https://assets.ccbp.in/frontend/react-js/cryptocurrency-bg.png"
+          alt="cryptocurrency"
+        />
+        {this.renderCryptocurrenciesView()}
+      </div>
+    )
+  }
+}
+
+export default CryptocurrenciesList
